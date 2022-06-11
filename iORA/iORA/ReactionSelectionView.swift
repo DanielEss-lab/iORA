9bec777bb48f23e6f0a03b24ca12d92d37b385bf
Jared Rossberg <jared.rossberg@gmail.com>
2022-06-10 18:38:59 -0600

    Remove extra files

--- iORA/iORA/ReactionSelectionView.swift
+++ iORA/iORA/ReactionSelectionView.swift
@@ -20,26 +20,19 @@ struct SubCategory {
 let subCategories = [
     SubCategory(name: "Reduction",
                 reactions: [
-                    ReactionFile(name: "AdN Red Acetone", filename: "AdN_Red_Acetone"),
-                    ReactionFile(name: "AdN Red Acetone (Updated)", filename: "AdN_Red_Acetone2")
+                    ReactionFile(name: "AdN Red Acetone", filename: "AdN_Red_Acetone")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Bronsted Acid-Base",
                 reactions: [
                     ReactionFile(name: "Acid-Base Acetone LDA", filename: "Acid-Base_Acetone_LDA"),
-                    ReactionFile(name: "Acid-Base Acetone LDA (Updated)", filename: "Acid-Base_Acetone_LDA2"),
-                    ReactionFile(name: "Acid-Base TFA Methoxide", filename: "Acid-Base_TFA_Methoxide"),
-                    ReactionFile(name: "Acid-Base TFA Methoxide (Updated)", filename: "Acid-Base_TFA_Methoxide2")
+                    ReactionFile(name: "Acid-Base TFA Methoxide", filename: "Acid-Base_TFA_Methoxide")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Alkyl Substitution",
                 reactions: [
                     ReactionFile(name: "SN1 Iodotertbutane", filename: "SN1_Iodotertbutane"),
-                    ReactionFile(name: "SN1 Iodotertbutane (Updated)", filename: "SN1_Iodotertbutane2"),
                     ReactionFile(name: "SN2 2-Bromobutane", filename: "SN2_2-Bromobutane"),
-                    ReactionFile(name: "SN2 2-Bromobutane (Updated)", filename: "SN2_2-Bromobutane2"),
                     ReactionFile(name: "SN2 Chloroethane Nonproductive", filename: "SN2_Chloroethane_nonproductive"),
-                    ReactionFile(name: "SN2 Chloroethane Nonproductive (Updated)", filename: "SN2_Chloroethane_nonproductive2"),
-                    ReactionFile(name: "SN2 Chloroethane", filename: "SN2_Chloroethane"),
-                    ReactionFile(name: "SN2 Chloroethane (Updated)", filename: "SN2_Chloroethane2")
+                    ReactionFile(name: "SN2 Chloroethane", filename: "SN2_Chloroethane")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Acyl Substitution",
                 reactions: [
@@ -47,49 +40,36 @@ let subCategories = [
     SubCategory(name: "Elimination",
                 reactions: [
                     ReactionFile(name: "E1cb PhenylChloroNitroPropane", filename: "E1cb_PhenylChloroNitroPropane"),
-                    ReactionFile(name: "E1cb PhenylChloroNitroPropane (Updated)", filename: "E1cb_PhenylChloroNitroPropane2"),
-                    ReactionFile(name: "E2 2-Bromobutane", filename: "E2_2-Bromobutane"),
-                    ReactionFile(name: "E2 2-Bromobutane (Updated)", filename: "E2_2-Bromobutane2")
+                    ReactionFile(name: "E2 2-Bromobutane", filename: "E2_2-Bromobutane")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Alkene Addition",
                 reactions: [
                     ReactionFile(name: "AdE Butene", filename: "AdE_Butene"),
-                    ReactionFile(name: "AdE Butene (Updated)", filename: "AdE_Butene2"),
                     ReactionFile(name: "AdE Hydroboration", filename: "AdE_Hydroboration"),
-                    ReactionFile(name: "AdE Hydroboration (Updated)", filename: "AdE_Hydroboration2"),
-                    ReactionFile(name: "AdE Carbene Addition", filename: "AdE_Carbene_addtion"),
-                    ReactionFile(name: "AdE Carbene Addition (Updated)", filename: "AdE_Carbene_addtion2")
+                    ReactionFile(name: "AdE Carbene Addition", filename: "AdE_Carbene_addtion")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Diene Addition",
                 reactions: [
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Rearrangement",
                 reactions: [
-                    ReactionFile(name: "Rearrangement MethylPentylCation", filename: "Rearrangement_MethylPentylCation"),
-                    ReactionFile(name: "Rearrangement MethylPentylCation (Updated)", filename: "Rearrangement_MethylPentylCation2")
+                    ReactionFile(name: "Rearrangement MethylPentylCation", filename: "Rearrangement_MethylPentylCation")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Radical",
                 reactions: [
                     ReactionFile(name: "AdR TertButyl", filename: "AdR_TertButyl"),
-                    ReactionFile(name: "AdR TertButyl (Updated)", filename: "AdR_TertButyl2"),
-                    ReactionFile(name: "HAT Butoxy", filename: "HAT_Butoxy"),
-                    ReactionFile(name: "HAT Butoxy (Updated)", filename: "HAT_Butoxy2")
+                    ReactionFile(name: "HAT Butoxy", filename: "HAT_Butoxy")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Pericyclic",
                 reactions: [
                     ReactionFile(name: "Cope Hexadiene", filename: "Cope_hexadiene"),
-                    ReactionFile(name: "Cope Hexadiene (Updated)", filename: "Cope_hexadiene2"),
                     ReactionFile(name: "DA Butadiene Ethene", filename: "DA_Butadiene_Ethene"),
-                    ReactionFile(name: "DA Butadiene Ethene (Updated)", filename: "DA_Butadiene_Ethene2"),
-                    ReactionFile(name: "Ringclosure Butadiene", filename: "Ringclosure_Butadiene"),
-                    ReactionFile(name: "Ringclosure Butadiene (Updated)", filename: "Ringclosure_Butadiene2")
+                    ReactionFile(name: "Ringclosure Butadiene", filename: "Ringclosure_Butadiene")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Conformational Change",
                 reactions: [
                     ReactionFile(name: "Conf Butane", filename: "Conf_Butane"),
-                    ReactionFile(name: "Conf Butane (Updated)", filename: "Conf_Butane2"),
-                    ReactionFile(name: "Conf Cyclohexane", filename: "Conf_Cyclohexane"),
-                    ReactionFile(name: "Conf Cyclohexane (Updated)", filename: "Conf_Cyclohexane2")
+                    ReactionFile(name: "Conf Cyclohexane", filename: "Conf_Cyclohexane")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Aromatic Substitution",
                 reactions: [
@@ -97,14 +77,11 @@ let subCategories = [
     SubCategory(name: "Oxidation",
                 reactions: [
                     ReactionFile(name: "AdE Epoxidation Z", filename: "AdE_Epoxidation_Z"),
-                    ReactionFile(name: "AdE Epoxidation Z (Updated)", filename: "AdE_Epoxidation_Z2"),
-                    ReactionFile(name: "AdE Epoxidation E", filename: "AdE_Epoxidation_E"),
-                    ReactionFile(name: "AdE Epoxidation E (Updated)", filename: "AdE_Epoxidation_E2")
+                    ReactionFile(name: "AdE Epoxidation E", filename: "AdE_Epoxidation_E")
                 ].sorted(by: { $0.name < $1.name })),
     SubCategory(name: "Carbonyl Addition",
                 reactions: [
-                    ReactionFile(name: "AdN Acetone", filename: "AdN_Acetone"),
-                    ReactionFile(name: "AdN Acetone (Updated)", filename: "AdN_Acetone2")
+                    ReactionFile(name: "AdN Acetone", filename: "AdN_Acetone")
                 ].sorted(by: { $0.name < $1.name }))
 ].sorted(by: { $0.name < $1.name })
 
