7b2abdd21d7b4eced3b43d5c97f301666fb1163f
Jared Rossberg <45674604+jaredrossberg@users.noreply.github.com>
2022-07-01 17:28:39 -0600

    v0.2 (5) (#14)
    * WIP: Settings menu (#13)
    
    * Copied over relevant changes from other branch to prevent merge conflicts
    
    * Fixed changing lighting and bg color
    
    * Add bond transparency setting
    
    * Bug fix
    
    * Fix button location
    
    * Add logic to determine infoview color
    
    * Not sure why this got undone

--- iORA/iORA/Defaults.swift
+++ iORA/iORA/Defaults.swift
@@ -19,8 +19,13 @@ class Defaults {
             //defaults.set(1.0, forKey: "DISTANCE_MULTIPLIER")
             defaults.set(1.0, forKey: "SCALE_FACTOR")
             defaults.set(0.06, forKey: "BOND_RADIUS")
-            defaults.set(0.04, forKey: "PARTIAL_BOND_RADIUS")
             defaults.set(false, forKey: "COLORED_BONDS")
+            
+            defaults.set(LightSources.ambient.rawValue, forKey: "LIGHT_SOURCE")
+            defaults.backgroundColor = UIColor(red: 0.09, green: 0.28, blue: 0.59, alpha: 1.00)
+            defaults.set(false, forKey: "ARE_BONDS_COLORED")
+            defaults.set(true, forKey: "ARE_BONDS_TRANSPARENT")
+            defaults.set(1.0, forKey: "ATOM_RADIUS_MULTIPLIER")
             
             //..
             defaults.set(true, forKey: "SET_UP_PERFORMED")
@@ -28,3 +33,55 @@ class Defaults {
     }
 }
 
+/* This code is StackOverflow and is used to allow storing colors inside UserDefaults. https://stackoverflow.com/questions/34366171/how-do-i-save-a-uicolor-with-userdefaults
+ */
+extension Numeric {
+    var data: Data {
+        var bytes = self
+        return Data(bytes: &bytes, count: MemoryLayout<Self>.size)
+    }
+}
+
+extension Data {
+    func object<T>() -> T { withUnsafeBytes{$0.load(as: T.self)} }
+    var color: UIColor { .init(data: self) }
+}
+
+extension UIColor {
+    convenience init(data: Data) {
+        let size = MemoryLayout<CGFloat>.size
+        self.init(red:   data.subdata(in: size*0..<size*1).object(),
+                  green: data.subdata(in: size*1..<size*2).object(),
+                  blue:  data.subdata(in: size*2..<size*3).object(),
+                  alpha: data.subdata(in: size*3..<size*4).object())
+    }
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
+        var (red, green, blue, alpha): (CGFloat, CGFloat, CGFloat, CGFloat) = (0, 0, 0, 0)
+        return getRed(&red, green: &green, blue: &blue, alpha: &alpha) ?
+        (red, green, blue, alpha) : nil
+    }
+    var data: Data? {
+        guard let rgba = rgba else { return nil }
+        return rgba.red.data + rgba.green.data + rgba.blue.data + rgba.alpha.data
+    }
+}
+
+extension UserDefaults {
+    func set(_ color: UIColor?, forKey defaultName: String) {
+        guard let data = color?.data else {
+            removeObject(forKey: defaultName)
+            return
+        }
+        set(data, forKey: defaultName)
+    }
+    func color(forKey defaultName: String) -> UIColor? {
+        data(forKey: defaultName)?.color
+    }
+}
+
+extension UserDefaults {
+    var backgroundColor: UIColor? {
+        get { color(forKey: "BG_COLOR") }
+        set { set(newValue, forKey: "BG_COLOR") }
+    }
+}
