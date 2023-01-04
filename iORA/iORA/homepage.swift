9782be0a5750ed0c7541f7bba0bcd6f6c29c43e9
Jared Rossberg <45674604+jaredrossberg@users.noreply.github.com>
2023-01-04 13:28:31 -0700

    Improve homepage design

--- iORA/iORA/homepage.swift
+++ iORA/iORA/homepage.swift
@@ -7,22 +7,66 @@
 
 import SwiftUI
 
+struct HomepageButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+    }
+}
+
 struct Homepage: View {
-    var moveToNextPage = false
+    @State private var isShowingReactionSelectionView = false
+    @State private var isShowingHowToView = false
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Homepage")
-                NavigationLink(destination: ReactionSelectionView()) {
-                    Text("Reactions")
-                }.navigationBarTitleDisplayMode(.inline)
-                NavigationLink(destination: TutorialPage()) {
-                    Text("How To Use")
-                }.navigationBarTitleDisplayMode(.inline)
+            VStack() {
+                NavigationLink(destination: TutorialPage(), isActive: $isShowingHowToView) { EmptyView() }
+                NavigationLink(destination: ReactionSelectionView(), isActive: $isShowingReactionSelectionView) { EmptyView() }
+                
+                Text("iORA")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Spacer()
+                
+                //HStack {
+                    Button("Reactions") {
+                        isShowingReactionSelectionView = true
+                    }.buttonStyle(HomepageButtonStyle())
+                    
+                    Button("How To") {
+                        isShowingHowToView = true
+                    }.buttonStyle(HomepageButtonStyle())
+                //}
+                
+                Spacer()
+                
+                /*List {
+                    Section {}
+                    Section {
+                        Button("Reactions") {
+                            isShowingReactionSelectionView = true
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                    Section {
+                        Button("How To") {
+                            isShowingHowToView = true
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                }
+                .listStyle(.insetGrouped)*/
+                
             }
             
         }
     }
+    
 }
 
 struct homepage_Previews: PreviewProvider {
